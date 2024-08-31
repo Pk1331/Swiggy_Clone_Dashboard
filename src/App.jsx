d08c563c1764111ebcf14d7e7f61d5013e66463f
PavanKumar1174 <andepavan37@gmail.com>
2024-08-31 16:14:36 +0530

    Fagito dashboard again

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import LandingPage from '../src/vendorDashboard/pages/LandingPage'
-import './app.css'
+import './App.css'
 import NotFound from './vendorDashboard/components/NotFound'
 const App = () => {
   return (
